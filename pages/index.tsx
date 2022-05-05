ef3942ff1eea62ebb487db8e2124eddd30efbe9e
JimDong <jimdong@damaiapp.com.tw>
2022-05-05 18:11:51 +0800

    testing prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,54 @@
-import type { NextPage } from "next";
+import type { NextPage, GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useState, useEffect } from "react";
 import { supabase } from "../utils/supabaseClient";
-import Auth from "../components/Auth";
-import Account from "../components/Account";
+// import Auth from "../components/Auth";
+// import Account from "../components/Account";
+import prisma from "../lib/prisma";
 
-const Home: NextPage = () => {
-  const [session, setSession] = useState(null);
+const Home: NextPage = ({ feed }) => {
+  return <>{feed.length}</>;
+  // const [session, setSession] = useState(null);
 
-  useEffect(() => {
-    setSession(supabase.auth.session());
+  // useEffect(() => {
+  //   setSession(supabase.auth.session());
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
+  //   supabase.auth.onAuthStateChange((_event, session) => {
+  //     setSession(session);
+  //   });
+  // }, []);
 
-  return (
-    <div className="container" style={{ padding: "50px 0 100px 0" }}>
-      {!session ? (
-        <Auth />
-      ) : (
-        <Account key={session.user.id} session={session} />
-      )}
-    </div>
-  );
+  // return (
+  //   <div className="container" style={{ padding: "50px 0 100px 0" }}>
+  //     {!session ? (
+  //       <Auth />
+  //     ) : (
+  //       <Account key={session.user.id} session={session} />
+  //     )}
+  //   </div>
+  // );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const feed = await prisma.post.findMany({
+    where: {
+      published: true,
+    },
+    include: {
+      author: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  console.log("feed", feed);
+  return {
+    props: { feed },
+  };
 };
 
 export default Home;
